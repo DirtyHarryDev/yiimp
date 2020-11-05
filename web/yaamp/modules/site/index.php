1167402dea4f50edae0b2f778d0e5a6ed097dbb6
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-11-05 01:11:37 +0100

    Update index.php

--- web/yaamp/modules/site/index.php
+++ web/yaamp/modules/site/index.php
@@ -145,7 +145,6 @@ $payout_freq = (YAAMP_PAYMENTS_FREQ / 3600) . " hours";
 <li><b>Your WALLET ADDRESS must be valid for the currency you mine</b>!</li>
 <li><b>DO NOT USE a BTC address here, the auto exchange is disabled on these stratums</b>!</li>
 <li>See the "dh-pool.xyz coins" area on the right for PORT numbers. You may mine any coin regardless if the coin is enabled or not for autoexchange. Payouts will only be made in that coins currency.</li>
-<li>Payouts are made automatically every hour for all balances above <b><?=$min_payout
 <br>
 </ul>
 </div></div></center><br>
