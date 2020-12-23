b84547a397a22f39c6ddf2779c21807e5ccbdc00
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-12-23 23:54:55 +0100

    Update payment.php

--- web/yaamp/core/backend/payment.php
+++ web/yaamp/core/backend/payment.php
@@ -58,7 +58,8 @@ function BackendCoinPayments($coin)
     $users = getdbolist('db_accounts', "balance>$min_payout AND coinid={$coin->id} ORDER BY balance DESC");
 
     // todo: enhance/detect payout_max from normal sendmany error
-    if ($coin->symbol == 'BOD' || $coin->symbol == 'DIME' || $coin->symbol == 'BTCRY' || !empty($coin->payout_max)) {
+    if($coin->symbol == 'BITC' || $coin->symbol == 'BNODE' || $coin->symbol == 'BOD' || $coin->symbol == 'DIME' || $coin->symbol == 'BTCRY' || $coin->symbol == 'IOTS' || $coin->symbol == 'ECC' || $coin->symbol == 'BCRS' || $coin->symbol == 'SAPP' || $coin->symbol == 'CURVE' || !empty($coin->payout_max))
+    {
         foreach ($users as $user) {
             $user = getdbo('db_accounts', $user->id);
             if (!$user)
